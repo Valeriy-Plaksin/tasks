7ba8626f24e3fc434cf2aeea939d5ec80ed9af8d
Valeriy-Plaksin <valeriy905@gmail.com>
2024-10-28 17:57:42 +0300

    func main

--- maih.py
+++ maih.py
@@ -2,3 +2,8 @@ print ("hellow world")
 for i in range(10)
     print("I love coding")
 print("I love coding a lot")    
+
+def main():
+    for i in range(10):
+        print("my love is Sberbank")
+        
